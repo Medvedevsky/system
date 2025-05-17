506e25b3ff89918e8631667f86f933e54948826f
Andrei Medvedevsky <amedvedevsky@syntes.io>
2025-05-17 19:57:22 +0700

    Update main.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,42 +3,41 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os/exec"
+	"runtime"
 	"time"
 
 	"github.com/TheTitanrain/w32"
 	"github.com/micmonay/keybd_event"
 )
 
-func main() {
-	rand.Seed(time.Now().UnixNano())
-	workLoop()
+var websites = []string{
+	"https://pixel.one",
+	"https://lk.clubpixel.ru/dashboard",
+	"https://ln1034.listokcrm.ru/",
+	"https://www.google.com/",
+	"https://dzen.ru/",
 }
 
-func workLoop() {
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	for {
-		sessionDuration := time.Duration(rand.Intn(120)+60) * time.Second
-		sessionEnd := time.Now().Add(sessionDuration)
-
-		fmt.Println("🟢 Начало", sessionDuration)
-
-		for time.Now().Before(sessionEnd) {
-			doRandomActivity()
-			time.Sleep(time.Duration(rand.Intn(5)+3) * time.Second)
-		}
-
-		breakTime := time.Duration(rand.Intn(60)+30) * time.Second
-		fmt.Println("🟡 Перерыв на", breakTime)
-		time.Sleep(breakTime)
+		doRandomActivity()
+		time.Sleep(30 * time.Second)
 	}
 }
 
 func doRandomActivity() {
-	action := rand.Intn(3)
-	switch action {
-	case 0:
+	// Примерно по 35% шанс для каждого действия
+	roll := rand.Intn(100)
+	switch {
+	case roll < 35:
 		moveMouseRandom()
-	case 1:
+	case roll < 70:
 		typeRandomKey()
+	default:
+		openRandomWebsite()
 	}
 }
 
@@ -46,30 +45,52 @@ func moveMouseRandom() {
 	x := rand.Intn(800) + 100
 	y := rand.Intn(600) + 100
 	w32.SetCursorPos(x, y)
-	fmt.Println("м:", x, y)
+	fmt.Println("🖱 Мышь перемещена в:", x, y)
 }
 
 func typeRandomKey() {
 	kb, err := keybd_event.NewKeyBonding()
 	if err != nil {
-		fmt.Println("keyboard error:", err)
+		fmt.Println("❌ Ошибка клавиатуры:", err)
 		return
 	}
 
 	keys := []int{
-		keybd_event.VK_A,
-		keybd_event.VK_S,
-		keybd_event.VK_D,
-		keybd_event.VK_F,
-		keybd_event.VK_SPACE,
+		keybd_event.VK_ENTER, keybd_event.VK_SPACE,
+		keybd_event.VK_A, keybd_event.VK_S,
+		keybd_event.VK_D, keybd_event.VK_F,
 		keybd_event.VK_TAB,
-		keybd_event.VK_ENTER,
 	}
-	k := keys[rand.Intn(len(keys))]
-	kb.SetKeys(k)
+
+	key := keys[rand.Intn(len(keys))]
+	kb.SetKeys(key)
 
 	if err := kb.Launching(); err != nil {
-		fmt.Println("key press error:", err)
+		fmt.Println("❌ Ошибка нажатия клавиши:", err)
+		return
 	}
-	fmt.Println("⌨", k)
+	fmt.Printf("⌨ Нажата клавиша: %d\n", key)
+}
+
+func openRandomWebsite() {
+	url := websites[rand.Intn(len(websites))]
+	fmt.Println("🌐 Открытие сайта:", url)
+	openBrowser(url)
+}
+
+func openBrowser(url string) {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
+	case "darwin":
+		cmd = exec.Command("open", url)
+	default: // Linux
+		cmd = exec.Command("xdg-open", url)
+	}
+
+	if err := cmd.Start(); err != nil {
+		fmt.Println("❌ Ошибка открытия сайта:", err)
+	}
 }
